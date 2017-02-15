37d10f562385e4fd8207aec786407dda88063832
Martyniuk <vl.martyniuk@gmail.com>
2017-02-15 10:29:32 +0200

    Initial changes in "marty.js" file according to ES6

--- src/static/js/marty.js
+++ src/static/js/marty.js
@@ -1,12 +1,10 @@
 /**
  * Created by Martynenko on 03.02.2017.
  */
-var Vacancy = function () {
+let Vacancy = () => {};
 
-};
-
-Vacancy.createFromData = function (data) {
-    var object = new Vacancy;
+Vacancy.prototype.createFromData = (data) => {
+    let object = new Vacancy;
     object.id = data.id;
     object.name = data.name;
     object.description = data.description;
@@ -18,16 +16,12 @@ Vacancy.createFromData = function (data) {
 };
 
 
-var Company = function () {
+let Company = () => {};
 
-};
+Company.prototype.addToHtml = () => {};
 
-Company.addToHtml = function () {
-
-};
-
-Company.createFromData = function (data) {
-    var object = new Company;
+Company.prototype.createFromData = (data) => {
+    let object = new Company;
     object.id = data.id;
     object.name = data.name;
     object.description = data.description;
@@ -37,12 +31,10 @@ Company.createFromData = function (data) {
     return object;
 };
 
-var Link = function () {
+let Link = () => {};
 
-};
-
-Link.createFromData = function (data) {
-    var object = new Link;
+Link.prototype.createFromData = (data) => {
+    let object = new Link;
     object.id = data.id;
     object.name = data.name;
     object.description = data.description;
@@ -51,19 +43,17 @@ Link.createFromData = function (data) {
     return object;
 };
 
-Link.addToHtml = function () {
+Link.prototype.addToHtml = () => {};
 
-};
-
-var links = [];
-var vacancies = [];
-var companies = [];
+let links = [],
+    vacancies = [],
+    companies = [];
 
 function getJsonOfVacancies() {
     let xhr = new XMLHttpRequest(),
         url = "http://138.68.77.7:8000/api/vacancy/";
     xhr.open("GET", url, true);
-    xhr.onreadystatechange = function () {
+    xhr.onreadystatechange = () => {
         if (xhr.readyState === 4) {
             let status = xhr.status;
             if (status >= 200 && status < 300 || status === 304) {
@@ -80,54 +70,51 @@ function getJsonOfVacancies() {
 function addVacancyElement(obj) {
     // создаем новый элемент div
     // и добавляем в него немного контента
-    var newDiv = document.createElement("div");
+    let newDiv = document.createElement("div");
     newDiv.setAttribute("class", "col-xs-4");
     newDiv.setAttribute("onclick", "show_more('vacancy', this.id)");
     newDiv.setAttribute("style", "cursor: pointer");
     newDiv.setAttribute("id", "vacancy-" + obj.id);
     newDiv.innerHTML = '<div class="title-box"><h4>' + obj.name + '</h4></div><div class="entry-container "><div class="entry-content"><p>' + obj.description.substr(0, 200) + '...</p></div></div></div>';
     // добавляем только что созданый элемент в дерево DOM
-    my_div = document.getElementById("vacancy-block");
-    my_div.appendChild(newDiv);
+    document.getElementById("vacancy-block").appendChild(newDiv);
 }
 
 function addLinkElement(obj) {
     // создаем новый элемент div
     // и добавляем в него немного контента
-    var newDiv = document.createElement("div");
+    let newDiv = document.createElement("div");
     newDiv.setAttribute("class", "col-xs-12");
     newDiv.setAttribute("id", "link-" + obj.id);
     newDiv.innerHTML = '<div class="usefull-link"><a href="' + obj.url + '"><p>' + obj.name + '</p></a><h6>' + obj.description.substr(0, 200) + '...</h6></div>';
     // добавляем только что созданый элемент в дерево DOM
-    my_div = document.getElementById("link-block");
-    my_div.appendChild(newDiv);
+    document.getElementById("link-block").appendChild(newDiv);
 }
 
 
 function addCompanyElement(obj) {
     // создаем новый элемент div
     // и добавляем в него немного контента
-    var newDiv = document.createElement("div");
+    let newDiv = document.createElement("div");
     newDiv.setAttribute("class", "col-xs-4");
     newDiv.setAttribute("onclick", "show_more('company', this.id)");
     newDiv.setAttribute("style", "cursor: pointer");
     newDiv.setAttribute("id", "company-" + obj.id);
     newDiv.innerHTML = '<div class="title-box"><h4>' + obj.name + '</h4></div><div class="entry-logo"><img src="' + obj.logo + '"></div><div class="entry-content">' + obj.description.substr(0, 200) + '...</div></div>';
     // добавляем только что созданый элемент в дерево DOM
-    my_div = document.getElementById("company-block");
-    my_div.appendChild(newDiv);
+    document.getElementById("company-block").appendChild(newDiv);
 }
 
 function show_more(type_api, id) {
     if (type_api == "vacancy") {
-        for (var vacancy in vacancies) {
+        for (let vacancy in vacancies) {
             if (vacancies[vacancy].id == id.split('-')[1]) {
                 showVacancyElement(vacancies[vacancy])
             }
         }
     }
     if (type_api == "company") {
-        for (var company in companies) {
+        for (let company in companies) {
             if (companies[company].id == id.split('-')[1]) {
                 showCompanyElement(companies[company]);
             }
@@ -135,29 +122,26 @@ function show_more(type_api, id) {
     }
 }
 
-
 function showCompanyElement(obj) {
     // создаем новый элемент div
     // и добавляем в него немного контента
-    var newDiv = document.createElement("div");
+    let newDiv = document.createElement("div");
     newDiv.setAttribute("class", "col-xs-4");
     newDiv.innerHTML = '<div class="title-box"><h4>' + obj.name + '</h4></div>' +
         '<div class="entry-image"><img src="/'+ obj.logo +'"></div>' +
         '<div class="entry-content">' + obj.description + '</div>';
     // добавляем только что созданый элемент в дерево DOM
-    my_div = document.getElementById("full-view");
-    my_div.innerHTML = newDiv.innerHTML;
+    document.getElementById("full-view").innerHTML = newDiv.innerHTML;
 }
 
 function showVacancyElement(obj) {
     // создаем новый элемент div
     // и добавляем в него немного контента
-    var newDiv = document.createElement("div");
+    let newDiv = document.createElement("div");
     newDiv.setAttribute("class", "col-xs-4");
     newDiv.innerHTML = '<div class="title-box"><h4>' + obj.name + '</h4></div>' +
         '<div><p>'+ obj.company_name +'</p></div>' +
         '<div class="entry-content">' + obj.description + '</div>';
     // добавляем только что созданый элемент в дерево DOM
-    my_div = document.getElementById("full-view");
-    my_div.innerHTML = newDiv.innerHTML;
+    document.getElementById("full-view").innerHTML = newDiv.innerHTML;
 }
