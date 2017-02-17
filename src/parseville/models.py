60822ba90a80d27a092150a87325205765da6bbc
martynenko.e <martynenko.evhen@gmail.com>
2017-02-17 20:21:31 +0200

    add new management commands improve views make admin page more comfortable change models

--- src/parseville/models.py
+++ src/parseville/models.py
@@ -27,6 +27,7 @@ class City(MetaModel):
 class ProgrammingLanguage(MetaModel):
     name = models.CharField(max_length=200, null=False, blank=False, db_index=True)
     alias = models.CharField(max_length=200, null=False, blank=False)
+    icon = models.ImageField(upload_to="language", blank=True, null=True)
     show = models.BooleanField(default=False)
 
 
@@ -34,28 +35,33 @@ class Company(MetaModel):
     name = models.CharField(max_length=200)
     alias = models.CharField(max_length=200)
     description = models.TextField(blank=True, null=True)
+    short_text = models.CharField(max_length=500, blank=True, null=True)
     logo = models.ImageField(upload_to="brand", blank=True, null=True)
     show = models.BooleanField(default=False)
     site_url = models.URLField(null=True, blank=True)
     vacancy_url = models.URLField(null=True, blank=True)
-    country = models.CharField(max_length=200, null=True, blank=True)
-    office = models.CharField(max_length=500, null=True, blank=True)
+    country = models.ForeignKey(Country, null=True, blank=True)
+    office = models.ManyToManyField(City)
     show_on_main = models.BooleanField(default=False)
+    has_parser = models.BooleanField(default=False)
     added_date = models.DateTimeField(auto_now_add=True)
+    extra = models.TextField(null=True, blank=True)
 
 
 class Vacancy(MetaModel):
     name = models.CharField(max_length=200)
     alias = models.CharField(max_length=200)
     description = models.TextField(blank=True, null=True)
+    short_text = models.CharField(max_length=500, blank=True, null=True)
     date_of_publication = models.DateField(blank=True, null=True)
     show = models.BooleanField(default=False)
     show_on_main = models.BooleanField(default=False)
     vacancy_url = models.URLField(null=True, blank=True)
     company = models.ForeignKey(Company)
-    city = models.CharField(max_length=200, null=True, blank=True)
-    programming_language = models.CharField(max_length=200, null=True, blank=True)
+    city = models.ForeignKey(City, null=True, blank=True)
+    programming_language = models.ForeignKey(ProgrammingLanguage, null=True, blank=True)
     added_date = models.DateTimeField(auto_now_add=True)
+    extra = models.TextField(null=True, blank=True)
 
 
 class UsefullLink(MetaModel):
@@ -66,4 +72,5 @@ class UsefullLink(MetaModel):
     show_on_main = models.BooleanField(default=False)
     url = models.URLField(null=True, blank=True)
     added_date = models.DateTimeField(auto_now_add=True)
+    extra = models.TextField(null=True, blank=True)
 
