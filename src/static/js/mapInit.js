886c3c30d2700e23e3cd5f4a7b3c032a183425f1
Martyniuk <vl.martyniuk@gmail.com>
2017-04-05 10:21:08 +0300

    1. Added 'use strict' 2. Added comments to each movement is done in InitMap to make it clear wtf is going on 3. Reworked some functions --> local variables swapped to global 4. Added interim functionality for infoWindow...that is empty for now.

--- src/static/js/mapInit.js
+++ src/static/js/mapInit.js
@@ -1,8 +1,9 @@
 /* Google API for Map */
-
+'use strict';
 var markers = [];
 
 function initMap() {
+    // as prior task is to query server in order to fill in GlobalMarkets(Array of JSON Objects) with a values
     $.ajax({
         url: window.location.protocol + '//' + window.location.hostname + ":" + window.location.port + '/api/office/',
         type: 'post',
@@ -12,7 +13,8 @@ function initMap() {
         async: false
     });
 
-    let uluru = {
+    // variables
+    var uluru = {
             lat: 50.4501,
             lng: 30.5234
         },
@@ -23,20 +25,45 @@ function initMap() {
             center: uluru,
             fullscreenControll: true
         },
-        map = new google.maps.Map(mapId, mapOptions);
+        map = new google.maps.Map(mapId, mapOptions),
+        infowindow = null;
 
-    markers = markersFromOffices(globalMarkers, addInfoWindow());
+    // converting Array of JSON objects to Array of Google API Marker objects
+    markers = markersFromOffices();
 
-    /*for (var i = 0; i < markers.length; i++) {
-        google.maps.event.addListener(markers[i], 'click', function () {
-            addInfoWindow().open(map, markers[i]);
-        })
-    }*/
+    // info Window google API for each Marker (infowindow is empty for now.....)
+    for (var i = 0; i < markers.length; i++) {
+        var marker = markers[i];
+        console.log('---------------------------------------------------> marker');
+        console.log(marker);
+        var sContent = '<div class="marker-info">' +
+            '<p>' + globalMarkers[i].company +
+            '</br>' +
+            globalMarkers[i].phone +
+            '</br>' +
+            globalMarkers[i].name +
+            '</br>' +
+            globalMarkers[i].address +
+            '</p>' + '</div>';
+        console.log('-----------------------------------------------------------------> S Content below');
+        console.log(sContent);
+
+        infowindow = new google.maps.InfoWindow({
+            // its empty for some reason
+            content: sContent
+        });
+
+        google.maps.event.addListener(marker, 'click', function () {
+            infowindow.setContent(this.html);
+            infowindow.open(map, this);
+        });
+    }
 
     let markerCluster = new MarkerClusterer(map, markers,
         {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
 
-    map.fitBounds(boundMap(globalMarkers));
+    //pass map into function is necessity otherwise it takes 'map' as come class from Python
+    AutoCenter(map);
 
     map.addListener('zoom_changed', function () {
         /*console.log('zoom');
@@ -53,15 +80,15 @@ function initMap() {
     });
 }
 
-function boundMap(officesArray) {
-    var bounds = getBoundsOfMap();
-    for (var key in officesArray) {
-        var latLng = new google.maps.LatLng(officesArray[key].lat, officesArray[key].lng);
-        bounds.extend(latLng);
-    }
-    return bounds;
+function AutoCenter(map) {
+    var bounds = new google.maps.LatLngBounds();
+    $.each(markers, function(index, marker) {
+        bounds.extend(marker.position);
+    });
+    map.fitBounds(bounds);
 }
 
+//agree that we need this function????
 function getBoundsOfMap() {
     return new google.maps.LatLngBounds({
         lat: 50.4501,
@@ -69,37 +96,36 @@ function getBoundsOfMap() {
     });
 }
 
-function addInfoWindow(obj) {
-    console.log(obj);
-    var infoWindow = {};
-    var sContent = '<h2>' + obj.company + '</h2>' +
-        '<bt/>' +
-        '<p>' +
-        obj.phone + ' ' +
-        '<bt/>' +
-        obj.name + ' ' +
-        '<bt/>' +
-        obj.address +
-        '</p>';
-    infoWindow = new google.maps.InfoWindow({
-        content: sContent
-    });
+/*function addInfoWindow(obj) {
+ console.log(obj);
+ var infoWindow = {};
+ var sContent = '<h2>' + obj.company + '</h2>' +
+ '<bt/>' +
+ '<p>' +
+ obj.phone + ' ' +
+ '<bt/>' +
+ obj.name + ' ' +
+ '<bt/>' +
+ obj.address +
+ '</p>';
+ infoWindow = new google.maps.InfoWindow({
+ content: sContent
+ });
 
-    return infoWindow;
-}
+ return infoWindow;
+ }*/
 
-function markersFromOffices(officesArray, func) {
-    return officesArray.map(function (marker) {
+function markersFromOffices() {
+    return globalMarkers.map(function (marker) {
         return new google.maps.Marker({
             position: {
                 lat: marker.lat,
                 lng: marker.lng
-            },
-            info: func(marker)
+            }
         });
     });
 }
-
+// check wtf below
 function fetchNewBounds(neLat, neLng, swLat, swLng) {
     $.ajax({
         url: window.location.protocol + '//' + window.location.hostname + ":" + window.location.port + '/api/get/office/',
@@ -116,7 +142,7 @@ function fetchNewBounds(neLat, neLng, swLat, swLng) {
         async: true
     });
 }
-
+//future functionality. Disabled for now
 function clearMarkers(markers) {
     if (markers) {
         for (var i = 0; i < markers.length; i++) {
