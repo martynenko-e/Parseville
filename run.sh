72fdf759b4e025fa4015a48499bcca1b331820fb
martynenko-e <martynenko.evhen@gmail.com>
2017-04-12 14:27:31 +0300

    add source path for production

--- run.sh
+++ run.sh
@@ -7,14 +7,16 @@ fi
 SOURCE_VOLUME="/home/marty/Projects/Parseville/src"
 STATIC_VOLUME="/home/marty/Projects/Parseville/src/collect_static"
 NGINX_VOLUME="/home/marty/Projects/Parseville/config/nginx"
+EXPOSE_PORT=8001
 
 if [ "$2" == "-p" ]; then
     SOURCE_VOLUME="/root/Parseville/src"
     STATIC_VOLUME="/root/Parseville/src/collect_static"
     NGINX_VOLUME="/root/Parseville/config/nginx"
+    EXPOSE_PORT=8000
 fi
 
 echo $SOURCE_VOLUME
 
 docker run -d --name parseville -v $SOURCE_VOLUME:/code parseville
-docker run -d --name pnginx -p 8001:8000 --link parseville:web -v $STATIC_VOLUME:/usr/share/nginx/html -v $NGINX_VOLUME:/etc/nginx/conf.d -d nginx
+docker run -d --name pnginx -p $EXPOSE_PORT:8000 --link parseville:web -v $STATIC_VOLUME:/usr/share/nginx/html -v $NGINX_VOLUME:/etc/nginx/conf.d -d nginx
