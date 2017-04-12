6e99b30a3716ea011c24cfa705ece9e379326087
martynenko-e <martynenko.evhen@gmail.com>
2017-04-12 14:25:53 +0300

    add source path for production

--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-
 if [ "$1" == "-b" ]; then
     docker build -t parseville .
 fi
@@ -8,6 +7,14 @@ fi
 SOURCE_VOLUME="/home/marty/Projects/Parseville/src"
 STATIC_VOLUME="/home/marty/Projects/Parseville/src/collect_static"
 NGINX_VOLUME="/home/marty/Projects/Parseville/config/nginx"
+
+if [ "$2" == "-p" ]; then
+    SOURCE_VOLUME="/root/Parseville/src"
+    STATIC_VOLUME="/root/Parseville/src/collect_static"
+    NGINX_VOLUME="/root/Parseville/config/nginx"
+fi
+
+echo $SOURCE_VOLUME
 
 docker run -d --name parseville -v $SOURCE_VOLUME:/code parseville
 docker run -d --name pnginx -p 8001:8000 --link parseville:web -v $STATIC_VOLUME:/usr/share/nginx/html -v $NGINX_VOLUME:/etc/nginx/conf.d -d nginx
